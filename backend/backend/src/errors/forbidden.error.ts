8a25e78517622a9458c7e3c5994fe30d477ad597
soumojit-D48 <soumojitdas107@gmail.com>
2025-12-20 02:55:57 +0530

    delete old structure

--- backend/backend/src/errors/forbidden.error.ts
+++
@@ -1,9 +0,0 @@
-import { BaseError } from './base.error'
-
-export class ForbiddenError extends BaseError {
-  constructor(message: string = 'Forbidden', metadata: Record<string, any> = {}) {
-    super(message, 403, 'FORBIDDEN', metadata)
-  }
-}
-
-
--- backend/backend/src/errors/unauthorized.error.ts
+++
@@ -1,7 +0,0 @@
-import { BaseError } from './base.error';
-
-export class UnauthorizedError extends BaseError {
-  constructor(message: string = 'Unauthorized', metadata: Record<string, any> = {}) {
-    super(message, 401, 'UNAUTHORIZED', metadata);
-  }
-}
